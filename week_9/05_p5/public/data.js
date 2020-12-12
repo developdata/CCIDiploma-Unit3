3052ffe46059ed531db425ea8c788d5adef15224
developdata <isk9@hotmail.com>
2020-12-12 12:03:07 +0000

    arduino data connected to p5.js

--- week_9/05_p5/public/data.js
+++ week_9/05_p5/public/data.js
@@ -3,6 +3,8 @@ let socket = io();
 
 let text = document.getElementById('text');
 
+let eWidth = 80;
+let eHeight = 80;
 
 // socket.on('welcome', (data)=> { REMOVE
 //     console.log(data);
@@ -10,6 +12,35 @@ let text = document.getElementById('text');
 // });
 
 socket.on('arduino data', (data)=>{
-    console.log(data)
+    // console.log(data)
     text.innerHTML = "The last button pressed is button number: " + data;
-})
+
+    if(data === "2"){
+        console.log('in 2')
+        if(eWidth < 120){
+            eWidth += 10;
+            eHeight += 10;
+        }
+    }
+
+    if(data === "3"){
+        if(eWidth > 10){
+            eWidth -= 10;
+            eHeight -= 10;
+        }
+    }
+});
+
+function setup(){
+    let canvas = createCanvas(400,400);
+    canvas.parent('p5Canvas');
+}
+
+ function draw(){
+    if (mouseIsPressed){
+        fill(0);
+    } else {
+        fill(255);
+    }
+    ellipse(mouseX, mouseY, eWidth, eHeight);
+}
